2b9376e1c95c12f49441ea052f3ca10091ee656e
Federico Barbieri <federicobarbieri54@gmail.com>
2023-04-07 15:54:23 +0200

    props

--- src/products.js
+++ src/products.js
@@ -1,8 +1,61 @@
+import product1 from './assets/product1.png';
+import product2 from "./assets/product2.png";
+import product3 from "./assets/product3.png";
+import product4 from "./assets/product4.png";
+import product5 from "./assets/product5.png";
+import product6 from "./assets/product6.webp";
+import product7 from "./assets/product7.webp";
+import product8 from "./assets/product8.webp";
+
+
 export const PRODUCTS = [
     {
         id: 1,
         productName: "Iphone",
         price: 999.0,
-        productImage,
-    }
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "MacBook",
+        price: 2000,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "Camera",
+        price: 600,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "jacket",
+        price: 200,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: "LED lights",
+        price: 60,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: "Sweatshirt",
+        price: 120,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: "Shirt",
+        price: 75,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: "Hat",
+        price: 30,
+        productImage: product8
+    },
+
 ]
