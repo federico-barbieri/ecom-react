5363a20d2114ffc688bb131a1686848f47df7c7f
Federico Barbieri <federicobarbieri54@gmail.com>
2023-04-08 12:20:00 +0200

    css

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {PRODUCTS} from '../../products';
 import {Product} from './product';
-import {} from "./shop.css";
+import "./shop.css";
 
 export const Shop = () => {
   return (
     <div className="shop">
         <div className="shopTitle">
-            <h1>Fede Tech Shop</h1>
+            <h1>KBH Tech Shop</h1>
         </div>
         <div className="products">
             {PRODUCTS.map((product) => (
