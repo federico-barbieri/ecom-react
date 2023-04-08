5ccdbea9fa535ab164013110f7c77c85fba7d5d9
Federico Barbieri <federicobarbieri54@gmail.com>
2023-04-08 12:02:58 +0200

    logic

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {} from '../products';
+import {createContext, useEffect, useState} from 'react'
+import {PRODUCTS} from '../products';
 
 export const ShopContext = createContext(null);
 
@@ -13,7 +13,39 @@ const getDefaultCart = () => {
 
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems){
+            if (cartItems[item] > 0){
+                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo.price;
+            }
+        }
+        return totalAmount;
+    }
+
+    const addToCart =  (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+    }
+
+    const removeFromCart =  (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
+    }
+
+    const updateCartItemCount = (newAmount, itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: newAmount}))
+    }
+
+    const contextValue = {
+        cartItems, 
+        addToCart, 
+        removeFromCart, 
+        updateCartItemCount,
+        getTotalCartAmount,
+    };
+
   return (
-    <ShopContext.Provider>{props.children}</ShopContext.Provider>
+    <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
   )
 }
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {ShopContext} from '../../context/shop-context';
 
 export const Product = (props) => {
     const {id, productName, price, productImage} = props.data;
+    const {addToCart, cartItems} = useContext(ShopContext);
+
+    const cartItemAmount = cartItems[id];
   return (
     <div className="product">
         <img src={productImage} />
         <div className="description">
             <p><strong>{productName}</strong></p>
             <p>${price}</p>
-            <button className="addToCartBtn">Add to cart</button>
+            <button className="addToCartBtn" onClick={() => addToCart(id)} >
+            Add to cart {cartItemAmount > 0 &&  <>({cartItemAmount})</>} </button>
         </div>
     </div>
   )
